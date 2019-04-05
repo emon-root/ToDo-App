c28a73dba364c1ed1ca87cc4e06ecb9103304c88
sujonict07 <sujonict07@gmail.com>
2019-04-05 23:54:15 +0600

    Added to-do list code

---
+++ app/src/test/java/com/shadhinsoft/todolist/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.shadhinsoft.todolist;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
